23385374ebe54dc0ce3b8b6f879a1ed88f629f6b
edustream <edustream1@gmail.com>
2024-06-12 16:57:10 +0200

    MAJ BDD
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch scotch
    # Your branch is up to date with 'origin/scotch'.
    #
    # Changes to be committed:
    #	modified:   index.php
    #

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
 
 <?php
 // Paramètres de connexion
-$host = 'localhost';
-$username = 'votre_nom_utilisateur';
-$password = 'votre_mot_de_passe';
-$database = 'nom_de_la_base_de_donnees';
+$host = '10.90.0.5';
+$username = 'vhugo';
+$password = 'P@ssw0rd77176';
+$database = 'bantko';
 
 // Connecter à la base de données MariaDB
 $db = new mysqli($host, $username, $password, $database);
@@ -24,7 +24,7 @@ if ($db->connect_error) {
 }
 
 // Sélectionner les données de la table
-$query = "SELECT nom, prenom, adresse FROM votre_table";
+$query = "SELECT Nom, Prénom, Service, FROM votre_table";
 $result = $db->query($query);
 
 // Vérifier le résultat de la requête
@@ -34,12 +34,12 @@ if (!$result) {
 
 // Afficher les données dans un tableau HTML
 echo "<table>";
-echo "<tr><th>Nom</th><th>Prénom</th><th>Adresse</th></tr>";
+echo "<tr><th>Nom</th><th>Prénom</th><th>Service</th></tr>";
 while ($row = $result->fetch_assoc()) {
     echo "<tr>";
-    echo "<td>" . htmlspecialchars($row['nom']) . "</td>";
-    echo "<td>" . htmlspecialchars($row['prenom']) . "</td>";
-    echo "<td>" . htmlspecialchars($row['adresse']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['Nom']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['Prénom']) . "</td>";
+    echo "<td>" . htmlspecialchars($row['Service']) . "</td>";
     echo "</tr>";
 }
 echo "</table>";
