626870d05ea6710945b3832bccd4902d821265e5
edustream <edustream1@gmail.com>
2024-06-14 10:55:26 +0200

    Update f_bdd.php

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -8,7 +8,7 @@ function connexionBDD()
 			DO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 			PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem'
 		);
-		$bdd = new PDO('mysql:host=bantko-bdd-gest.mysql.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8', 'usergs', 'mdpGS', $options);
+		$bdd = new PDO('mysql:host=bantko-bdd-gest.mysql.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8', 'vhugo', 'P@ssw0rdP@ssw0rd', $options);
 		return $bdd;
 	}
 	catch(Exception $e)
