53e1a5af70d09602059eb428a0efe0155cb6abe1
edustream <edustream1@gmail.com>
2024-06-14 10:23:44 +0200

    Update f_bdd.php

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -7,7 +7,7 @@ function connexionBDD()
 		$options = array(
 			PDO::MYSQL_ATTR_SSL_CA => 'ssl/DigiCertGlobalRootG2.crt.pem'
 		);
-		$bdd = new PDO('mysql:host=bantko-bdd-gest.mysql.database.azure.com', 'usergs', 'mdpGS', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION), $options);
+		$bdd = new PDO('mysql:host=servbd;port=3306;dbname=bdd_geststages;charset=utf8', 'usergs', 'mdpGS', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION), $options);
 		return $bdd;
 	}
 	catch(Exception $e)
